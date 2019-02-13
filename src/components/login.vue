b6be6e111146dbdab70f56e134bca1201dceed90
wlcgit <602664407@qq.com>
2019-02-13 18:07:51 +0800

    登录组件

---
+++ src/components/login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login-wrap">
+    <el-form
+     class="login-form"
+     label-position="top" label-width="80px" :model="formdata">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名">
+     <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+     <el-input v-model="formdata.password"></el-input>
+     </el-form-item>
+     <el-button class="login-btn" type="primary">登录</el-button>
+</el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-form{
+    background-color: #ffffff;
+    width: 400px;
+    border-radius: 5px;
+    padding: 30px;
+
+}
+.login-btn{
+    width: 100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'login',
       path: '/',
-      name: 'login'
+      component: Login
     }
   ]
 })
