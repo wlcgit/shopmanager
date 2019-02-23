b796bf4ff9af8372bb4eae5a25a924ce7efa7c75
wlcgit <602664407@qq.com>
2019-02-23 16:56:36 +0800

    ”完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,49 +1,59 @@
 <template>
-    <div class="login-wrap">
-    <el-form
-     class="login-form"
-     label-position="top" label-width="80px" :model="formdata">
-        <h2>用户登录</h2>
-        <el-form-item label="用户名">
-     <el-input v-model="formdata.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-     <el-input v-model="formdata.password"></el-input>
-     </el-form-item>
-     <el-button
-     @click.prevent ="handlelogin()"
-     class="login-btn" type="primary">登录</el-button>
-</el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form :rules="rules" ref="ruleForm" class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="handlelogin('ruleForm')" class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在3到5个字符", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "change" }]
       }
-    }
+    };
   },
   methods: {
-    async handlelogin () {
-      const res = await this.$http.post(`login`, this.formdata)
-      console.log(res)
-      const { data: { data, meta: { msg, status } } } = res
+    async handlelogin(ruleForm) {
+      this.$refs[ruleForm].validate(async valid => {
+        if (valid) {
+          const res = await this.$http.post(`login`, this.formdata);
+          console.log(res);
+          const { data: { data, meta: { msg, status } } } = res;
 
-      if (status === 200) {
-        localStorage.setItem('token', data.token)
-        this.$router.push({
-          name: 'home'
-        })
-      } else {
-        this.$message.error(msg)
-      }
+          if (status === 200) {
+            localStorage.setItem("token", data.token);
+            this.$router.push({
+              name: "home"
+            });
+          } else {
+            this.$message.error(msg);
+          }
+        } else {
+          this.$message.warning("验证失败");
+          return false;
+        }
+      });
     }
   }
-}
+};
 </script>
 
 <style>
